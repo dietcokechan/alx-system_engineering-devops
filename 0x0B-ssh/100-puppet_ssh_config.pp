8b00e957d68938371bf348fc0a8e1006245abec6
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-01-21 23:56:58 +0200

    ssh config using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# ssh config using puppet
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}

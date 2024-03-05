3232e4f76f8b68fc34d6d4b23b8675dfdaf2db3e
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-03-05 22:05:06 +0200

    how many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""how many subs"""
+from requests import get
+from sys import argv
+
+def number_of_subscribers(subreddit):
+    """return number of subreddit subs"""
+    header = {"User-Agent": "diet coke"}
+    subs = get("https://www.reddit.com/r/{}/about.json".format(
+        subreddit), headers=header).json()
+
+    try:
+        return subs.get("data").get("subscribers")
+    except:
+        return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])

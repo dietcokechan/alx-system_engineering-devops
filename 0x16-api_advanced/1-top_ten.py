8973414b9c6a347dd794a9c82162c6e6988ed766
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-03-05 22:23:55 +0200

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""top ten"""
+from requests import get
+
+
+def top_ten(subreddit):
+    """get top ten"""
+    try:
+        posts = get('https://www.reddit.com/r/{}/hot.json?count=10'.format(
+            subreddit)).json().get('data').get('children')
+        print('\n'.join([p.get('data').get('title')
+                        for p in posts][:10]))
+    except(Exception):
+        print("None")

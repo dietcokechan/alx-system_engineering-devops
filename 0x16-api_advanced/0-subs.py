f2e1c2918464f4483480af2be888fdf0fc5b31c6
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-03-05 22:13:12 +0200

    removed header and import

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """how many subs"""
 from requests import get
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
     """return number of subreddit subs"""
-    header = {"User-Agent": "diet coke"}
     subs = get("https://www.reddit.com/r/{}/about.json".format(
-        subreddit), headers=header).json()
-
+        subreddit)).json()
     try:
         return subs.get("data").get("subscribers")
     except(Exception):

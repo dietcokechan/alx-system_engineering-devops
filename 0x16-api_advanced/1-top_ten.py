1c668ab6295988e215575f8e1a0e3d4509d5b2f7
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-03-06 10:00:16 +0200

    added header

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,10 +5,12 @@ from requests import get
 
 def top_ten(subreddit):
     """get top ten"""
+    header = {'User-Agent': 'Diet Coke'}
     try:
         posts = get('https://www.reddit.com/r/{}/hot.json?count=10'
                     .format(subreddit),
-                    allow_redirects=False).json().get('data').get('children')
+                    allow_redirects=False,
+                    headers=header).json().get('data').get('children')
         print('\n'.join([p.get('data').get('title')
                         for p in posts]))
     except Exception:

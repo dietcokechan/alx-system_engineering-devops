02b9e102456d1e73a864305ffe2cf745440da24d
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-03-06 09:48:38 +0200

    if statement instead of try except

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,7 @@ from requests import get
 def number_of_subscribers(subreddit):
     """return number of subreddit subs"""
     subs = get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), allow_redirects=False).json()
-    try:
-        return subs.get('data').get('subscribers')
-    except Exception:
+        subreddit), allow_redirects=False)
+    if subs.status_code >= 300:
         return 0
+    return subs.json().get('data').get('subscribers')

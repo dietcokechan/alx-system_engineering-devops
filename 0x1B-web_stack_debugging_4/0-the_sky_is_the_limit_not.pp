6150c1bbce8c9ee78fa5fecd65f9bf372f6eb683
dietcokechan <dietcokechan@gmail.com>
2024-03-14 11:07:07 +0000

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # sky is the limit
 exec {'sky is the limit':
-  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
-  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-  onlyif  => 'test -f /etc/default/nginx'
+  command  => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx && sudo service nginx restart',
+  provider => shell,
 }
